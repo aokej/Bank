4e85cedc8ecf122532ad7d11ec26993c137ee822
Garbus0 <aleksanderolszycki11@gmail.com>
2025-10-29 20:44:34 +0100

    Logger.java

--- src/Logger.java
+++ src/Logger.java
@@ -2,30 +2,35 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public interface Logger {
-    void log(String message);
-}
-
-class InfoLogger implements Logger {
-    @Override
+class InfoLogger {
     public void log(String message) {
         String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         System.out.println("[INFO " + time + "] " + message);
     }
 }
 
-class WarnLogger implements Logger {
-    @Override
+class WarnLogger {
     public void log(String message) {
         String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         System.out.println("[UWAGA " + time + "] " + message);
     }
 }
 
-class ErrorLogger implements Logger {
-    @Override
+class ErrorLogger {
     public void log(String message) {
         String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         System.out.println("[BŁĄD " + time + "] " + message);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        InfoLogger info = new InfoLogger();
+        WarnLogger warn = new WarnLogger();
+        ErrorLogger error = new ErrorLogger();
+
+        info.log("Info");
+        warn.log("Uwaga");
+        error.log("Błąd");
     }
 }
